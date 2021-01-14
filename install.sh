e93d3172af99c7296351ad9ec9085f11f4b732fa
FEI YUAN <fei.yuan@bcm.edu>
2021-01-14 15:36:44 -0600

    Fixed some errors in install.py, works fine for now

--- install.sh
+++ install.sh
@@ -3,35 +3,39 @@
 set -e
 unset PYTHONPATH
 
-venv="$( cd .; pwd -P )"/venv
+motif="$( cd .; pwd -P )"
+venv="${motif}"/venv
 echo "Set up virtual environment for motif pipeline ...";
 
 if [ ! -f "${venv}/bin/python" ]
+  then
   echo "    Installing Python (3.8) ...";
   conda create --prefix="${venv}" --yes --quiet python=3.8 >/dev/null
   echo "    Successful installed Python (3.8)."
 fi
 
 if [ ! -f "${venv}/bin/perl" ]
+  then
   echo "    Installing perl ...";
   conda install --channel=bioconda --prefix="${venv}" --yes --quiet perl
   echo "    Successful installed Perl."
 fi
 
 if [ ! -f "${venv}/bin/python" ]
+  then
   echo "    Installing homer ...";
   conda install --channel=bioconda --prefix="${venv}" --yes --quiet homer >/dev/null
   echo "    Successful installed homer."
 fi
 
 echo "    Finalizing installation ...";
-cp "${venv}/source/annotate_peaks_bedformat_with_proxdistal_lncRNA.pl" "${venv}/bin/"
+cp "${motif}/source/annotate_peaks_bedformat_with_proxdistal_lncRNA.pl" "${venv}/bin/"
 chmod +x "${venv}/bin/annotate_peaks_bedformat_with_proxdistal_lncRNA.pl"
 
-cp "${venv}/source/pull_seqs_for_regions_and_run_homer_clip_analysis_version.pl" "${venv}/bin/"
+cp "${motif}/source/pull_seqs_for_regions_and_run_homer_clip_analysis_version.pl" "${venv}/bin/"
 chmod +x "${venv}/bin/pull_seqs_for_regions_and_run_homer_clip_analysis_version.pl"
 
-cp "${venv}/source/motif.py" "${venv}/bin/"
+cp "${motif}/source/motif.py" "${venv}/bin/"
 
 read -r -d '' script << EOF || true
 #!/usr/bin/env bash
@@ -41,13 +45,13 @@ export TMPDIR=/storage/vannostrand/tmp
 export TEMP=/storage/vannostrand/tmp
 export TMP=/storage/vannostrand/tmp
 
-export PATH="${venv}/bin:$PATH
+export PATH="${venv}"/bin:\$PATH
 
 python ${venv}/bin/motif.py \$@
 EOF
 
-echo "${script}" > "${clip}/motif"
-chmod +x "${clip}/motif"
+echo "${script}" > "${motif}/motif"
+chmod +x "${motif}/motif"
 echo "    Successfully finalized installation."
 
 echo "Successfully set up virtual environment for motif pipeline in ${venv}."
